074845c5753e3b0ff47b2c0eb86ccfa8d9cbaab1
Volodymyr Kamuz <v.kamuz@gmail.com>
2024-11-06 21:12:14 +0200

    Add more validation rules

--- script.js
+++ script.js
@@ -30,8 +30,22 @@ function checkRequired(inputArr) {
 }
 
 // Validate email
-function isEmail(input) {
-    return String(input).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(input.value)) {
+        showError(input, 'Email is not valid');
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check password match
+function checkPasswordMatch(input1, input2) {
+    if ( input1.value !== input2.value ) {
+        showError(input2, 'Password is not match');
+    } else {
+        showSuccess(input2);
+    }
 }
 
 // Get field name
@@ -39,9 +53,26 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// Check length input value
+function checkLength(input, min, max) {
+    if ( input.value.length < min ) {
+        showError(input, `${getFieldName(input)} must be at least ${min}`);
+    } else if ( input.value.length > max ) {
+        console.log(input.value)
+        showError(input, `${getFieldName(input)} must be less than ${max}`);
+    } else {
+        showSuccess(input);
+    }
+}
+
 // Submit form
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 30);
+    checkEmail(email);
+    checkPasswordMatch(password, password2);
 });
+
